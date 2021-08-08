86830451bc882092c2b8c1930b31ab09a02616e9
koushik <koushik.radhakrishnan92@gmail.com>
2021-08-08 14:29:04 +0800

    Update: Gradle dependencies added

---
+++ MovieDB/app/src/main/java/com/example/moviedb/ui/movieList/MovieListActivity.kt
@@ -0,0 +1,12 @@
+package com.example.moviedb.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.moviedb.R
+
+class MovieListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie_list)
+    }
+}
