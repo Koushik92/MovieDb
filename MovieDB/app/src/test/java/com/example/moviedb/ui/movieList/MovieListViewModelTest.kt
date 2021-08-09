060a2c4b56d6f0262b1b709875fbd9ef1a18cb1f
koushik <koushik.radhakrishnan92@gmail.com>
2021-08-09 14:14:41 +0800

    Update : Movie List Unit Test by Release Date added

--- MovieDB/app/src/test/java/com/example/moviedb/ui/movieList/MovieListViewModelTest.kt
+++ MovieDB/app/src/test/java/com/example/moviedb/ui/movieList/MovieListViewModelTest.kt
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
-internal class MovieListViewModelTest {
+ class MovieListViewModelTest {
 
     private lateinit var mMovieListViewModel : MovieListViewModel
 
@@ -25,6 +25,11 @@ internal class MovieListViewModelTest {
         assertEquals(getSortedMovieListNames(),mMovieListViewModel.sortMovieByName(getRandomMovieListNames()))
     }
 
+     @Test
+     fun testMovieListByDate(){
+         assertEquals(getSortedMovieListDate(),mMovieListViewModel.sortMovieByDate(getRandomMovieListByDate()))
+     }
+
     private fun getRandomMovieListNames(): ArrayList<Movie> {
         var randomMovieListNames = ArrayList<Movie>()
         randomMovieListNames.add(Movie(id =1,name = "A"))
@@ -33,10 +38,27 @@ internal class MovieListViewModelTest {
         return randomMovieListNames
     }
 
+   private fun getRandomMovieListByDate() : ArrayList<Movie>{
+       var randomMovieListNames = ArrayList<Movie>()
+       randomMovieListNames.add(Movie(id =1,release_date = "1/2/2021"))
+       randomMovieListNames.add(Movie(id =2,release_date = "8/4/2021"))
+       randomMovieListNames.add(Movie(id =3,release_date = "2/2/2021"))
+       return randomMovieListNames
+   }
+
     private fun getSortedMovieListNames(): ArrayList<Movie> {
-        var randomMovieListNames = ArrayList<Movie>()
-        randomMovieListNames.add(Movie(id =1,name = "A"))
-        randomMovieListNames.add(Movie(id =3,name = "E"))
-        randomMovieListNames.add(Movie(id =2,name = "Z"))
-        return randomMovieListNames
-    }}
+        var sortedMovieListNames = ArrayList<Movie>()
+        sortedMovieListNames.add(Movie(id =1,name = "A"))
+        sortedMovieListNames.add(Movie(id =3,name = "E"))
+        sortedMovieListNames.add(Movie(id =2,name = "Z"))
+        return sortedMovieListNames
+    }
+
+     private fun getSortedMovieListDate(): ArrayList<Movie> {
+         var sortedMovieListDate = ArrayList<Movie>()
+         sortedMovieListDate.add(Movie(id =1,release_date = "1/2/2021"))
+         sortedMovieListDate.add(Movie(id =3,release_date = "2/2/2021"))
+         sortedMovieListDate.add(Movie(id =2,release_date = "8/4/2021"))
+         return sortedMovieListDate
+     }
+ }
